07a7acc1a437492218566960c99aba8c89aa5795
shaun-saisi <saisishaun4@gmail.com>
2023-10-05 19:06:09 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Anunda Shaun <saisishaun4@gmail.com>
+ */
+#include <stdio.h>
+
+/**
+ *main - Prints "with proper grammar, 
+ *               but the outcome is a piece of art"
+ *
+ *
+ * REturn: Always 0.
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,");
+
+     return(0);
+}	
