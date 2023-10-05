343a5e4c79f52608350a4ab7bdd56b6072eec249
shaun-saisi <saisishaun4@gmail.com>
2023-10-05 18:56:43 +0000

    4-puts-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,19 @@
 /*
- * File : 4-puts.c
+ * File: 4-puts.c
  * Auth: Anunda Shaun <saisishaun4@gmail.com>
  */
 
 #include <stdio.h>
 
-/*
- * main - Prints "Programming is like building multilingual puzzle, followed by a new line
+/**
+ * main - Prints "Programming is like building multilingual puzzle,
+ * 		 followed by a new line."
  *
  * Return: always 0.
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }	
