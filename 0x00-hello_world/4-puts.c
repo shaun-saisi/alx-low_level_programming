d7368adca73845de7794ab4bbf96d458c37afae9
shaun-saisi <saisishaun4@gmail.com>
2023-10-05 18:52:14 +0000

    4-puts-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
-/* A basic c program to print out using puts*/
+/*
+ * File : 4-puts.c
+ * Auth: Anunda Shaun <saisishaun4@gmail.com>
+ */
 
 #include <stdio.h>
+
+/*
+ * main - Prints "Programming is like building multilingual puzzle, followed by a new line
+ *
+ * Return: always 0.
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
+
 	return(0);
 }	
