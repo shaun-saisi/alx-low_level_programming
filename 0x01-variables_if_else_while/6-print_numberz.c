e5a50684a0f32b046c10fdd1c7cb356b9f35795b
shaun-saisi <saisishaun4@gmail.com>
2023-10-06 19:38:15 +0000

    6-print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints out all single digit numbers of base 10
+ * Return: Akways(Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for(numbers = 10; numbers < 10; numbers++)
+	putchar(numbers);
+
+	return (0);	
+}	
