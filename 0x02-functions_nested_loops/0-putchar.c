2b01123873b8237fe2285bd0881b5ff723ab22e8
shaun-saisi <saisishaun4@gmail.com>
2023-10-11 20:47:23 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints out putchar followed by a new line
+ * return: Always(success)
+ */
+int main(){
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+	
