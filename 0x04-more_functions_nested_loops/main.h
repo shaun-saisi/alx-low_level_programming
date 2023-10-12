e05e6dfbf108387a74b2c8e8064489adc67b2d35
shaun-saisi <saisishaun4@gmail.com>
2023-10-12 15:19:29 +0000

    0 -is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void print_triangle(int size);
+void print_number(int n);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int mul(int a, int b);
+int _isupper(int c);
+int _isdigit(int c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
