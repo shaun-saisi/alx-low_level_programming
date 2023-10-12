fda73136c3167934a1df5c11863a61293b1c4a8b
shaun-saisi <saisishaun4@gmail.com>
2023-10-12 18:40:58 +0000

    9- fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main (void)
 	{
 		if (a % 3 == 0 && a % 5 != 0)
 		{
-		printf("Fizz", '\n');
+		printf("Fizz");
 		}
 		else if (a % 5 == 0 && a % 3 != 0)
 		{
-		printf("Buzz", '\n');
+		printf("Buzz");
 		}
 		else if (a % 3 == 0 && a % 5 ==0)
 		{
-		printf("FizzBuzz", '\n');
+		printf("FizzBuzz");
 		}
 		else
 		{
-		printf("%d", a, '\n');
-		}	
+		printf("%d", a);
+		}
 	}
-}	
+}
