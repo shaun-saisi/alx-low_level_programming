c8954da3ab870de02dd971d5a297e2c9dc593bd7
shaun-saisi <saisishaun4@gmail.com>
2023-10-16 14:18:36 +0000

    0-reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/* void reset_to_98: takes a pointer to an int
+ *@n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This contains programs on C- pointers , arrays and strings.. Very important concepts to understand if one has to be good in C
