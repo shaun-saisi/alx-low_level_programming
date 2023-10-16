cb55608f0a349ccc0bc2e4ec9e7b7d90aef1539a
shaun-saisi <saisishaun4@gmail.com>
2023-10-16 15:51:48 +0000

    4 - print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,15 @@ void print_rev(char *s)
 	int i;
 	int length = 0;
 
-	while (s[length] != '\0')
+	while (*s != '\0')
 		{
 		length++;
-		i--;
+		s++;
 		}
 
-	for (i = length - 1; i >= 0; i--)
+	for (i = length; i >= 0; i--)
 		{
-		_putchar(s[i]);
+		_putchar(*s);
 		s--;
 		}
 
