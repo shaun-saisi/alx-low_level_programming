c3dfa5bacb2cc05d305f7cb1ade1e3a236dc5c9f
shaun-saisi <saisishaun4@gmail.com>
2023-10-16 15:17:24 +0000

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _puts -function prints a string followed by a new line
+ * @str: String to be printed
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	if (str != NULL)
+	{
+	puts(str);
+	}
+}
